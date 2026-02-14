99cc492e02e89fa1e73529b2d75f06b2a1c50531
Matthew Avarello <182387652+MatthewAvarello@users.noreply.github.com>
2026-02-13 23:13:43 -0500

    (feat) implemented includes, insert, getnode and get parent node function to Tree class

--- Tree.js
+++ Tree.js
@@ -38,4 +38,99 @@ export default class Tree{
         let rootNode = new Node(leftNode,rightNode,middleValue)
         return rootNode
     }
+    includes(value){
+        let currentnode = this.root;
+        while(currentnode !== null){
+            if(currentnode.value == value){
+                return true
+            }          
+            if (value > currentnode.value){
+                currentnode = currentnode.right
+            } else {
+                currentnode = currentnode.left
+            }
+        }
+        return false
+    }
+    insert(value){
+        if (this.includes(value)){
+            return
+        }
+        let currentnode = this.root
+        do{
+            if (value > currentnode.value){
+                if (currentnode.right == null){
+                    currentnode.right = new Node(undefined,undefined,value)
+                    console.log("Inserting")
+                    return
+                }
+                currentnode = currentnode.right
+            } else if (value < currentnode.value){
+                if(currentnode.left == null){
+                    console.log("Inserting")
+                    currentnode.left = new Node(undefined,undefined,value)
+                    return
+                }
+                currentnode = currentnode.left
+            }
+        } while(currentnode !== null)
+    }
+    getNode(value){
+        if(this.includes(value) == false){
+            return
+        }
+        let currentnode = this.root;
+        while(currentnode !== null){
+            if(currentnode.value == value){
+                return currentnode
+            }          
+            if (value > currentnode.value){
+                currentnode = currentnode.right
+            } else {
+                currentnode = currentnode.left
+            }
+        }
+    }
+    getParentNode(value){
+        if(this.includes(value) == false){
+            return
+        }
+        if (this.root.value == value){
+            return
+        }
+        let currentnode = this.root 
+        while(currentnode !== null){
+            try {
+                if (currentnode.left.value == value){
+                    return currentnode.value
+                }
+            } catch (error) {
+ 
+            }
+            try {
+                if (currentnode.right.value == value){
+                    return currentnode.value
+                }
+            } catch (error) {
+
+            }
+            if (currentnode.left.value == value || currentnode.right.value == value){
+                return currentnode.value
+            }
+
+            if (value > currentnode.value){
+                currentnode = currentnode.right
+            } else {
+                currentnode = currentnode.left
+            }
+        }
+
+    }
+    deleteItem(value){
+        if (!this.includes(value)){
+            return
+        }
+
+
+    }
 }
--- index.js
+++ index.js
@@ -1,4 +1,14 @@
+import prettyPrint from "./PrettyPrint.js";
 import Tree from "./Tree.js";
 
 let myTree = new Tree([1,5,4,8,9,7,2,3,6,54,67,90,12,13,1123,445,7654,89,99,56])
-console.log(myTree.root)
+prettyPrint(myTree.root)
+console.log("Does the tree include 27? " + myTree.includes(27))
+myTree.insert(27)
+console.log("Does the tree include 27? " +myTree.includes(27))
+myTree.insert(27)
+myTree.insert(10000)
+myTree.insert(11)
+prettyPrint(myTree.root)
+console.log(myTree.getNode(5))
+console.log(myTree.getParentNode(10000))
